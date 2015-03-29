a241a652693b3cbaca458d123994f7a24495921d
Doug Preston <dgprstn@outlook.com>
2015-03-29 10:22:13 +0000

    Changes made by Doug Preston (dgprstn@outlook.com).

--- RTMWarehouseApp/formIssueAgainstGoldDeposit/formIssueAgainstGoldDepositForm.js
+++ RTMWarehouseApp/formIssueAgainstGoldDeposit/formIssueAgainstGoldDepositForm.js
@@ -4,8 +4,41 @@ app.models.formIssueAgainstGoldDeposit.formIssueAgainstGoldDepositForm = (functi
             amtTendered: undefined,
             fboAccount: undefined,
         },
-        submit: function() {},
-        cancel: function() {}
+        submit: function() {
+            //app.models.formIssueAgainstGoldDeposit.formIssueAgainstGoldDepositForm.get("amtTendered");
+            //app.models.formIssueAgainstGoldDeposit.formIssueAgainstGoldDepositForm.get("fboAccount");
+            // use ripple-lib to send amtTendered XAU to fboAccount
+            
+        },
+        cancel: function() {
+            // unused
+        },
+        doscan: function() {
+            var that = this;
+            if (window.navigator.simulator === true) {
+                //alert("Not Supported in Simulator.");
+                $("#fbo").val("rAAAAAAABBBBBBBCCCCCCCDDDDDDDEEEEE");
+                app.models.formIssueAgainstGoldDeposit.formIssueAgainstGoldDepositForm.set("fboAccount", "rAAAAAAABBBBBBBCCCCCCCDDDDDDDEEEEE");                
+                
+            }
+            else {
+                cordova.plugins.barcodeScanner.scan(
+                    function(result) {
+                        if (!result.cancelled) {
+                            var regex = /(r[0-9a-zA-Z]{33,33}$)/i;  // public key format
+                            //var regex = /(s[0-9a-zA-Z]{28,28}$)/i;    // secret key format
+                            var matches = result.text.match(regex);                            
+                            if (matches.length > 0) {
+                                $("#fbo").val(matches[0]);
+                                app.models.formIssueAgainstGoldDeposit.formIssueAgainstGoldDepositForm.set("fboAccount", matches[0]); 
+                            }
+                        }
+                    }, 
+                    function(error) {
+                        //safelog("Scanning failed: " + error);
+                    });
+            }
+    }
     });
     return viewModel;
 })();
--- RTMWarehouseApp/formView/formViewForm.js
+++ RTMWarehouseApp/formView/formViewForm.js
@@ -2,14 +2,39 @@ app.models.formView.formViewForm = (function() {
     var viewModel = kendo.observable({
         model: {
             skeyIssuer: undefined,
+            pkeyIssuer: undefined,
+            uriRipple: function (){
+                return "ripple:" + app.models.formView.formViewForm.get("pkeyIssuer");
+            },
+            scanVisible: false,
         },
         submit: function() {
-            alert("logint");
+            if ($("#session").text() == "Logout"){
+                alert("logout code goes here for " + app.models.formView.formViewForm.get("skeyIssuer"));
+                // use ripple-lib to terminate session
+                app.models.formView.formViewForm.set("skeyIssuer", undefined);
+                app.models.formView.formViewForm.set("pkeyIssuer", undefined);
+                $("#skey").val(undefined);
+                $("#session").text("Login");      
+                //app.models.formView.formViewForm.set("scanVisible", !app.models.formView.formViewForm.get("scanVisible"));
+            }else{
+                alert("login code goes here for " + app.models.formView.formViewForm.get("skeyIssuer"));
+                // use ripple-lib to login using skeyIssuer
+                // discover pkeyIssuer from session
+                app.models.formView.formViewForm.set("pkeyIssuer", "rAAAAAAABBBBBBBCCCCCCCDDDDDDDEEEEE");
+                $("#session").text("Logout");
+                //app.models.formView.formViewForm.set("scanVisible", !app.models.formView.formViewForm.get("scanVisible"));
+                alert("uri="+app.models.formView.formViewForm.model.uriRipple());
+                app.navigate("#main");
+            }
         },
         doscan: function() {
             var that = this;
             if (window.navigator.simulator === true) {
-                alert("Not Supported in Simulator.");
+                //alert("Not Supported in Simulator.");
+                $("#skey").val("sAAAAAAABBBBBBBCCCCCCCDDDDDDD");
+                app.models.formView.formViewForm.set("skeyIssuer", "sAAAAAAABBBBBBBCCCCCCCDDDDDDD");                
+                app.models.formView.formViewForm.set("pkeyIssuer", "rAAAAAAABBBBBBBCCCCCCCDDDDDDDEEEEE"); 
             }
             else {
                 cordova.plugins.barcodeScanner.scan(
@@ -17,11 +42,10 @@ app.models.formView.formViewForm = (function() {
                         if (!result.cancelled) {
                             //var regex = /(r[0-9a-zA-Z]{33,33}$)/i;  // public key format
                             var regex = /(s[0-9a-zA-Z]{28,28}$)/i;    // secret key format
-                            var matches = result.text.match(regex);
-                            alert(matches);
+                            var matches = result.text.match(regex);                            
                             if (matches.length > 0) {
-                                //safelog("set merchant: "+matches[0]);
-                                that._setMerchantAccount(matches[0]); 
+                                $("#skey").val(matches[0]);
+                                app.models.formView.formViewForm.set("skeyIssuer", matches[0]); 
                             }
                         }
                     }, 
@@ -33,5 +57,7 @@ app.models.formView.formViewForm = (function() {
 
         
     });
+    
+    //kendo.bind($("#formViewForm"), viewModel);
     return viewModel;
 })();
